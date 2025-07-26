497847867b5d82337048e5f853faaa38e293bac3
Shay Shahar <shayscal90@gmail.com>
2025-07-26 19:26:54 +0300

    code rework + apollo support

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const TuyaAccessory = require('./lib/TuyaAccessory');
 const TuyaDiscovery = require('./lib/TuyaDiscovery');
 const AirConditionerAccessory = require('./lib/AirConditionerAccessory');
-const AirConditionerSferaAccessory = require('./lib/AirConditionerSferaAccessory');
-
 
 const PLUGIN_NAME = 'homebridge-tadiran-ac-extended';
 const PLATFORM_NAME = 'TadiranACExtended';
 
 const CLASS_DEF = {
 	default: AirConditionerAccessory,
-	sfera: AirConditionerSferaAccessory
 };
 
 let Characteristic, PlatformAccessory, Service, Categories, UUID;
@@ -151,7 +148,7 @@ class TadiranAC {
 
 	addAccessory(device) {
 		const deviceConfig = device.context;
-		const type = device.type = device.context.model || 'mini-central';
+		const type = 'default'
 
 		const Accessory = CLASS_DEF[type];
 
